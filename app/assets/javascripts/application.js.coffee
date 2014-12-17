fe4dbeb4b62222600e6b2e89bad2a2e7d2901202
Petr Balaban <balaban.petr@gmail.com>
2014-12-17 23:15:27 +0200

    Tooltip added for domain dates.

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -1,3 +1,6 @@
 #= require jquery
 #= require jquery-ujs
 #= require bootstrap-sass-official
+
+$ ->
+  $('[data-toggle="tooltip"]').tooltip()
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def date_tooltip_for date
+    return nil if date.blank?
+
+    opts = { toggle: :tooltip, title: date.strftime('%I:%M %p') }
+    content_tag(:span, date.strftime('%b %d, %Y'), data: opts)
+  end
+
+  def date_closing_tooltip_for date
+    return nil if date.blank?
+    return date_tooltip_for(date) if date > 1.day.since
+
+    if date < 1.hour.since
+      opts = { toggle: :tooltip, title: date.strftime('%I:%M %p') }
+      content_tag(:span, time_ago_in_words(date), data: opts)
+    else
+      opts = { toggle: :tooltip, title: 'Today' }
+      content_tag(:span, date.strftime('%I:%M %p'), data: opts)
+    end
+  end
 end
